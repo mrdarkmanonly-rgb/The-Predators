81a8a54688d0d020488511d72ee3c65736a6f4c8
devta6 <dev343057@gmail.com>
2026-09-10 01:56:00 +0530

    Complete project update

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const page = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center text-5xl font-extrabold">Jeee Baat</div>
+  )
+}
+
+export default page
